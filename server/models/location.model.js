1bb906d2f17f476bae7f6e0739dd518171472fe5
Morden Elton John <134627073+Elton133@users.noreply.github.com>
2025-10-28 12:14:21 +0000

    Revert "Server"

--- server/models/location.model.js
+++
@@ -1,8 +0,0 @@
-import { db } from "../config/db.js";
-export const findByIds = async (userIds) => {
-  const placeholders = userIds.map(() => "?").join(",");
-  const query = `SELECT * FROM users WHERE user_id IN (${placeholders})`;
-
-  const [rows] = await db.execute(query, userIds);
-  return rows;
-};
